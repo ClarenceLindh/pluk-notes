cc1859df419b4ae7bea492724fe2aae4556c3725
Peter <p.akesson@gmail.com>
2020-11-05 15:27:40 +0100

    Jag la till datum att visas i render.note
    ..

--- src/www/main.js
+++ src/www/main.js
@@ -19,7 +19,7 @@ async function renderNotes() {
         {
             let noteLi = `<li id="${note.id}"><span class="note-title" onclick="">
             ${note.title}</span><button onclick="">Delete</button><br>
-            <div class="note-content">${note.content}</div><br></li>`;
+            <div class="note-content">${note.content}</div><br><br><div class="note-date">${note.date}</div></li>`;
             noteList.innerHTML += noteLi;
         }
     }
