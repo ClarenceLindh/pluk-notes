a6c990d49d4b7adc5d4027a04554f0b8e38d3cdb
Stefan <Stefan_mijatovic97@hotmail.com>
2020-11-05 12:28:19 +0100

    Added java express and connection in db
    com#4

--- src/Database.java
+++ src/Database.java
@@ -1,2 +1,14 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Database {
+    private Connection conn;
+    public Database() {
+        try {
+            conn = DriverManager.getConnection("jdbc:sqlite:PlukNotes.db");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,27 @@
+import express.Express;
+import express.middleware.Middleware;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Main {
-    
+
+    public static void main(String[] args) {
+        // write your code here
+
+        Express app = new Express();
+
+
+        try {
+            app.use(Middleware.statics(Paths.get("src/www").toString()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        app.listen(3000);//defaults to port 80
+        System.out.println("Server started on port 3000");
+    }
 }
+
