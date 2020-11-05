90022b14d2342c65b22f102bae92f7cbb662c765
Stefan <Stefan_mijatovic97@hotmail.com>
2020-11-05 12:56:15 +0100

    Const/get/set/toString
    Add constructor/getter setter and to string in notes

--- src/Note.java
+++ src/Note.java
@@ -1,3 +1,66 @@
 public class Note {
+int id;
+String date;
+String title;
+String  content;
+Boolean archived;
 
+    public Note(int id, String date, String title, String content, Boolean archived) {
+        this.id = id;
+        this.date = date;
+        this.title = title;
+        this.content = content;
+        this.archived = archived;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Boolean getArchived() {
+        return archived;
+    }
+
+    public void setArchived(Boolean archived) {
+        this.archived = archived;
+    }
+
+    @Override
+    public String toString() {
+        return "Note{" +
+                "id=" + id +
+                ", date='" + date + '\'' +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", archived=" + archived +
+                '}';
+    }
 }
