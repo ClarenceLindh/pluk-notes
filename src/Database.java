25b5b0857b041f0d21d0b763d1cb20e26873accf
Stefan <Stefan_mijatovic97@hotmail.com>
2020-11-05 13:10:28 +0100

    Added methods in Database
    Added get notes, get notes by id and Create notes in database

--- src/Database.java
+++ src/Database.java
@@ -1,6 +1,8 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import express.utils.Utils;
+
+import java.sql.*;
+import java.util.List;
 
 public class Database {
     private Connection conn;
@@ -11,4 +13,59 @@ public class Database {
             throwables.printStackTrace();
         }
     }
+
+    public List<Note> getNotes(){
+        List<Note> notes =  null;
+        try {
+            PreparedStatement stmt=conn.prepareStatement("SELECT * FROM notes");
+            ResultSet rs=stmt.executeQuery();
+
+            Note[] notesFromRS= (Note[]) Utils.readResultSetToObject(rs , Note[].class);
+            notes = List.of(notesFromRS);
+
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return notes;
+    }
+
+    public  Note getNotesById (int id){
+
+        Note note = null;
+
+        try {
+            PreparedStatement stmt = conn.prepareStatement("SELECT * FROM notes WHERE id = ?");
+            stmt.setInt(1, id);
+            ResultSet rs= stmt.executeQuery();
+
+            Note[]notesFromRs = (Note[]) Utils.readResultSetToObject(rs, Note[].class);
+
+
+            note = notesFromRs[0];
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return note;
+
+    }
+
+    public void createNote ( Note note){
+        try {
+            PreparedStatement stmt = conn.prepareStatement("INSERT INTO users ( name, age) VALUES (?,?)");
+            stmt.setString(1,note.getTitle());
+            stmt.setString(2,note.getContent());
+
+            stmt.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+
+    }
 }
+
